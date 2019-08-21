7fb378d3d8475b841788f6de1614f292a7d00ea5
Zhijin Chen <mail@chenzhijin.com>
2019-08-22 00:12:29 +0900

    Converted to python3 with 2to3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from __future__ import division
+
 import sky_area
 from input_file import InputFile
 from diagram import Diagram
@@ -6,12 +6,12 @@ from coord_calc import CoordCalc
 
 f = InputFile('stardata.csv')
 
-area = sky_area.SKY_AREA_ORION
+area = sky_area.SKY_AREA_NORTH
 star_data_list = f.get_stars(area)
 
 cc = CoordCalc(star_data_list, area, 500)
 cc.process()
 
 d = Diagram('My Star Map', area, star_data_list)
-map(d.add_curve, cc.calc_curves())
+list(map(d.add_curve, cc.calc_curves()))
 d.render_svg('star-chart.svg')
